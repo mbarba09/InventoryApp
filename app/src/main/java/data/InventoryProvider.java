ad66abfa4ae5e75e543704a745f66b2374916727
Matthew Barba <mbarba009@gmail.com>
2018-09-13 12:41:31 -0700

    Insert Method

--- app/src/main/java/data/InventoryProvider.java
+++ app/src/main/java/data/InventoryProvider.java
@@ -1,34 +1,190 @@
 package data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 public class InventoryProvider extends ContentProvider {
-    @Override
-    public boolean onCreate() {
-        return false;
+
+    /** Tag for the log messages */
+    public static final String LOG_TAG = InventoryProvider.class.getSimpleName();
+
+
+    /** URI matcher code for the content URI for the pets table */
+    private static final int ITEM = 100;
+
+    /** URI matcher code for the content URI for a single pet in the pets table */
+    private static final int ITEM_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        // The content URI of the form "content://com.example.matthewbarba.inventoryapp/item" will map to the
+        // integer code {@link #ITEM}. This URI is used to provide access to MULTIPLE rows
+        // of the inventory table.
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_ITEMS, ITEM);
+
+        // The content URI of the form "content://com.example.matthewbarba.inventoryapp/item/#" will map to the
+        // integer code {@link #ITEM_ID}. This URI is used to provide access to ONE single row
+        // of the inventory table.
+        //
+        // In this case, the "#" wildcard is used where "#" can be substituted for an integer.
+        // For example, "content://com.example.matthewbarba.inventoryapp/item/3" matches, but
+        // "content://com.example.matthewbarba.inventoryapp/item" (without a number at the end) doesn't match.
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_ITEMS + "/#", ITEM_ID);
     }
 
-    @Nullable
+    /** Database helper object */
+    private InventoryDbHelper mDbHelper;
+
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+    public boolean onCreate() {
+        mDbHelper = new InventoryDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // This cursor will hold the result of the query
+        Cursor cursor;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEM:
+                // For the ITEM code, query the pets table directly with the given
+                // projection, selection, selection arguments, and sort order. The cursor
+                // could contain multiple rows of the pets table.
+                cursor = database.query(InventoryContract.InventoryEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case ITEM_ID:
+                // For the ITEM_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.example.matthewbarba.inventoryapp/item/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = InventoryContract.InventoryEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                // This will perform a query on the inventory table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(InventoryContract.InventoryEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+
+        // Set notification URI on the Cursor,
+        // so we know what content URI the Cursor was created for.
+        // If the data at this URI changes, then we know we need to update the Cursor.
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return the cursor
+        return cursor;
     }
 
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEM:
+                return InventoryContract.InventoryEntry.CONTENT_LIST_TYPE;
+            case ITEM_ID:
+                return InventoryContract.InventoryEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
     }
 
-    @Nullable
     @Override
-    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEM:
+                return insertPet(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Insert a pet into the database with the given content values. Return the new content URI
+     * for that specific row in the database.
+     */
+    private Uri insertPet(Uri uri, ContentValues values) {
+        // Check that the product name is not null
+        String name = values.getAsString(InventoryContract.InventoryEntry.COLUMN_PRODUCT_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Item requires a name");
+        }
+
+        // Check that the supplier name is not null
+        String supplier = values.getAsString(InventoryContract.InventoryEntry.COLUMN_SUPPLIER);
+        if (supplier == null) {
+            throw new IllegalArgumentException("The supplier requires a name");
+        }
+
+        // Check that the supplier phone number is not null
+        String supplierPhoneNumber = values.getAsString(InventoryContract.InventoryEntry.COLUMN_SUPPLIER_NUMBER);
+        if (supplierPhoneNumber == null) {
+            throw new IllegalArgumentException("Supplier Phone # is required");
+        }
+
+        // Check that the price is greater or equal to $0
+        Integer price = values.getAsInteger(InventoryContract.InventoryEntry.COLUMN_PRICE);
+        if (price != null || price < 0) {
+            throw new IllegalArgumentException("A valid price is required");
+        }
+
+        // If the quantity is entered, check that it's greater than or equal to 0.
+        Integer quantity = values.getAsInteger(InventoryContract.InventoryEntry.COLUMN_QUANTITY);
+        if (quantity != null && quantity < 0) {
+            throw new IllegalArgumentException("A valid quantity is required");
+        }
+
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Insert the new item with the given values
+        long id = database.insert(InventoryContract.InventoryEntry.TABLE_NAME, null, values);
+        // If the ID is -1, then the insertion failed. Log an error and return null.
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        // Notify all listeners that the data has changed for the pet content URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return the new URI with the ID (of the newly inserted row) appended at the end
+        return ContentUris.withAppendedId(uri, id);
     }
 
     @Override
