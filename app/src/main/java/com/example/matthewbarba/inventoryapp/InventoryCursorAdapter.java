044b919a4f8baa2fc5bfc97cdc50f67a366c88e4
Matthew Barba <mbarba009@gmail.com>
2018-09-13 12:11:27 -0700

    Cursor Adapter and List Item Layout add

---
+++ app/src/main/java/com/example/matthewbarba/inventoryapp/InventoryCursorAdapter.java
@@ -0,0 +1,69 @@
+package com.example.matthewbarba.inventoryapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import data.InventoryContract;
+
+public class InventoryCursorAdapter extends CursorAdapter{
+
+    /**
+     * Constructs a new {@link InventoryCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public InventoryCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the inventory data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current item can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        // Find the individual views that we want to modify in the list item layout
+        TextView productNameTextView = (TextView) view.findViewById(R.id.list_product_name);
+        TextView priceTextView = (TextView) view.findViewById(R.id.list_price);
+
+        // Find the columns of the item details that we are interested in
+        int productNameColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRICE);
+
+        // Read the item details from the Cursor for the current pet
+        String productName = cursor.getString(productNameColumnIndex);
+        String price = cursor.getString(priceColumnIndex);
+
+        //update the TextViews with the attributes for the current item
+        productNameTextView.setText(productName);
+        priceTextView.setText(price);
+
+    }
+}
